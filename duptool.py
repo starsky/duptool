92eb5d8a7d769224d5eabbe104afb751ae18359d
Michal Koperski <michal@itiner.pl>
2013-10-27 22:49:16 +0100

    Clean up command, tmp_dir

--- duptool.py
+++ duptool.py
@@ -4,11 +4,17 @@ import argparse
 import os
 import json
 import subprocess
+from urlparse import urlparse
+import logging
+import datetime
+
+LOG_LEVEL = logging.DEBUG
 
 def main():
     parser = argparse.ArgumentParser(description="A duplicity helper for backups.")
     parser.add_argument('-c','--config',help='Config file location.')
     parser.add_argument('-g','--group',help='Group name.')
+    parser.add_argument('-l','--log_dir',help='Directory to save log file.')
     sub_parsers = parser.add_subparsers(dest='subparser_name')
     backup_parser = sub_parsers.add_parser('backup',help='preform backup')
     restore_parser = sub_parsers.add_parser('restore',help='preform restore')
@@ -24,35 +30,91 @@ def main():
         group = args.group
     else:
         group = None
-
-    print config_file
-    print 'Processing %s group(s)' % (group if group is not None else 'all')
+    log_file_name = ('%s.log') % datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    if args.log_dir:
+        logging.basicConfig(filename=os.path.join(args.log_dir,log_file_name), level=LOG_LEVEL)
+    else:
+        home = os.path.expanduser("~")
+        logging.basicConfig(filename=os.path.join(home,log_file_name), level=LOG_LEVEL)
+    logging.debug('Config file: %s' % config_file)
+    logging.debug('Processing %s group(s)' % (group if group is not None else 'all'))
 
     if args.subparser_name == 'backup':
-        print 'Performing backup...'
+        logging.debug('Performing backup...')
         __backup__(config_file,group)
     else:
-        print 'Performing restore...'
+        logging.debug('Performing restore...')
 
 def __backup__(config_file,group=None):
     json_data=open(config_file).read()
     config = json.loads(json_data)
+
+    env_var = os.environ.copy()
+    env_var['PASSPHRASE'] = config['encryption_key']
+
+
     groups = config['groups']
     if group is not None:
         if group in [g['name'] for g in groups]:
             groups = [g for g in groups if g['name'] == group]
         else:
-            print "No such group %s in config" % group
+            err_msg = "No such group %s in config" % group
+            print err_msg
+            logging.error(err_msg)
             exit(-100)
+    global_status = True
     for g in groups:
-        print "Backing up %s" % g['name']
+        status = True
+        logging.info("Backing up %s" % g['name'])
+        if not os.path.exists(urlparse(g['dest_dir']).path):
+            err_msg = '[Error] no dest dir %s' % g['dest_dir']
+            print err_msg
+            logging.error(err_msg)
+            status = False
+            continue
+        #Backup
         CMD =  ['duplicity']
         CMD.extend(g['duplicity_opts'])
         CMD.append(g['source_dir'])
         CMD.append(g['dest_dir'])
-        print CMD
-        p = subprocess.Popen(CMD, stdin=subprocess.PIPE)
-
+        if config.has_key('tmp_dir'):
+            CMD.append('--tempdir')
+            CMD.append(config['tmp_dir'])
+        logging.debug('CMD: %s' % CMD)
+        p = subprocess.Popen(CMD,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=env_var)
+        (stdout,stderr) = p.communicate()
+        ret_code = p.returncode
+        status &= ret_code == 0
+        logging.info('Duplicity backup finished with code: %d' % ret_code)
+        logging.info(stdout)
+        logging.info(stderr)
+        #Verifying backup
+        VERIFY_CMD = ['duplicity','verify',g['dest_dir'],g['source_dir']]
+        p = subprocess.Popen(VERIFY_CMD,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=env_var)
+        (stdout,stderr) = p.communicate()
+        ret_code = p.returncode
+        status &= ret_code == 0
+        logging.info('Duplicity verify finished with code: %d' % ret_code)
+        logging.info(stdout)
+        logging.info(stderr)
+        #Clean up command
+        if g.has_key('clean_cmd'):
+            CLEAN_CMD = ['duplicity']
+            CLEAN_CMD.extend(g['clean_cmd'])
+            CLEAN_CMD.append(g['dest_dir'])
+            p = subprocess.Popen(CLEAN_CMD,stdout=subprocess.PIPE,stderr=subprocess.PIPE,env=env_var)
+            (stdout,stderr) = p.communicate()
+            ret_code = p.returncode
+            status &= ret_code == 0
+            logging.info('Duplicity clean up finished with code: %d' % ret_code)
+            logging.info(stdout)
+            logging.info(stderr)
+        #Whole group process status
+        result_msg = 'SUCESS' if status else 'FAILED'
+        logging.info('Backing up %s %s' % (g['name'], result_msg))
+        logging.info('==========================================')
+        global_status &= status
+    return global_status
 
 def __get_home__():
     home = os.path.expanduser("~")
